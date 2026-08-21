b0a691b0a6a29643012aa8ccd5317fb9da09023a
Pranjal soni <pranjalsoni484@gmail.com>
2026-08-21 10:53:09 +0530

    update root

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function RootPage() {
-  redirect("/admin/login");
+  return(
+    <main>
+      <div>
+        <div><Link href="/admin/login">admin</Link></div>
+        <div><Link href="/cashier/login">cashier</Link></div>
+        <div><Link href="/inventory/login">inventory</Link></div>
+      </div>
+    </main>
+  )
 }
